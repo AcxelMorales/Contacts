e7eb6b623ee201195527331a9ec8263974f6ddb7
Acxel Morales <acxelmorales97@gmail.com>
2021-11-21 21:30:27 -0600

    New contact functionality added

--- app/src/main/java/com/example/contacts/NewContactActivity.kt
+++ app/src/main/java/com/example/contacts/NewContactActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 
 import androidx.appcompat.widget.Toolbar
+import com.example.contacts.classes.Contact
 
 class NewContactActivity : AppCompatActivity() {
 
@@ -29,6 +31,28 @@ class NewContactActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item?.itemId) {
             R.id.action_add_contact -> {
+                val name = findViewById<EditText>(R.id.detailName)
+                val surname = findViewById<EditText>(R.id.detailSurname)
+                val company = findViewById<EditText>(R.id.detailCompany)
+                val years = findViewById<EditText>(R.id.detailAge)
+                val weight = findViewById<EditText>(R.id.detailWeight)
+                val telephone = findViewById<EditText>(R.id.detailTelephone)
+                val email = findViewById<EditText>(R.id.detailEmail)
+                val address = findViewById<EditText>(R.id.detailAddress)
+
+                MainActivity.addContact(Contact(
+                    name.text.toString(),
+                    surname.text.toString(),
+                    company.text.toString(),
+                    years.text.toString().toInt(),
+                    weight.text.toString().toFloat(),
+                    address.text.toString(),
+                    telephone.text.toString(),
+                    email.text.toString(),
+                    R.drawable.foto_01
+                ))
+
+                finish()
                 true
             } else -> {
                 super.onOptionsItemSelected(item)
